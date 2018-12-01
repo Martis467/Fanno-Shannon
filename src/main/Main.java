e7c018a98acab5fe5e57f613115e57b0d7f5593f
DESKTOP-712L91G\Martis <martynas.jakimcikas@gmail.com>
2018-12-01 14:57:15 +0200

    Adding UI

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -6,11 +6,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.net.URL;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        URL resource = Main.class.getClassLoader().getResource("sample/sample.fxml");
+        Parent root = FXMLLoader.load(resource);
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
