e604eece1c6ebfd74b55dfb25f79c9d025dad01b
Tomasv388 <34406656+Tomasv388@users.noreply.github.com>
2018-12-05 11:28:04 +0200

    added better separator calculation

--- src/main/models/Encode.java
+++ src/main/models/Encode.java
@@ -10,7 +10,7 @@ public class Encode {
     //Tracks how often a appears in our map
     private HashMap<String, Integer> freqMap = new HashMap<String, Integer>();
 
-    private HashMap<String, String> compressedResult = new HashMap<String, String>();
+    private HashMap<String, String> codeTable = new HashMap<String, String>();
 
     private String fileExtension;
     private int wordLength;
@@ -46,14 +46,14 @@ public class Encode {
                 //convert bits to string
                 String bitString = convertBitsToBitString(fileBuffer, readBytes);
                 getFreqTable(bitString, wordLength);
-                //System.out.println(freqMap);
             }
             fs.close();
+            //System.out.println(freqMap);
 
             ArrayList<Map.Entry<String, Integer>> sortedFreqList = sortedByFreq();
             //System.out.println(sortedFreqList);
-            compressString(sortedFreqList);
-            //System.out.println(compressedResult);
+            getCodeTable(sortedFreqList);
+            //System.out.println(codeTable);
 
             saveEncodedFile(filepath);
 
@@ -66,18 +66,18 @@ public class Encode {
 
     private void resetDefaults (){
         freqMap = new HashMap<String, Integer>();
-        compressedResult = new HashMap<String, String>();
+        codeTable = new HashMap<String, String>();
         decodedTextRoot = "";
         if (bufferSize % wordLength != 0)
             bufferSize += (wordLength - (bufferSize % wordLength));
     }
 
     private String convertBitsToBitString(byte[] byteBuffer, int size) {
-        String bitBufferStr = "";
+        StringBuilder bitBufferStr = new StringBuilder();
         for (int i = 0; i < size; ++i) {
-            bitBufferStr += String.format("%8s", Integer.toBinaryString(byteBuffer[i] & 0xFF)).replace(' ', '0');
+            bitBufferStr.append(String.format("%8s", Integer.toBinaryString(byteBuffer[i] & 0xFF)).replace(' ', '0'));
         }
-        return bitBufferStr;
+        return bitBufferStr.toString();
     }
 
     private String getFileName(URL filepath) throws URISyntaxException {
@@ -110,37 +110,48 @@ public class Encode {
         return list;
     }
 
-    private void compressString(ArrayList<Map.Entry<String, Integer>> sortedFreqList) {
-        List<String> bitList = new ArrayList<String>();
-
-        for (Map.Entry<String, Integer> entry : sortedFreqList) {
-            bitList.add(entry.getKey());
-        }
-
-        appendBit(compressedResult, bitList, true);
+    private void getCodeTable(ArrayList<Map.Entry<String, Integer>> sortedFreqList) {
+        appendBit(codeTable, sortedFreqList, true);
     }
 
-    private void appendBit(HashMap<String, String> result, List<String> wordList, boolean up) {
+    private void appendBit(HashMap<String, String> result, List<Map.Entry<String, Integer>> wordList, boolean up) {
 
         String bit = "";
         if (!result.isEmpty()) {
             bit = (up) ? "0" : "1";
         }
 
-        for (String word : wordList) {
-            String s = (result.get(word) == null) ? "" : result.get(word);
-            result.put(word, s + bit);
+        for (Map.Entry<String, Integer> word : wordList) {
+            String s = (result.get(word.getKey()) == null) ? "" : result.get(word.getKey());
+            result.put(word.getKey(), s + bit);
         }
 
         if (wordList.size() >= 2) {
-            int separator = (int) Math.floor((float) wordList.size() / 2.0);
+            int separator = getSeparator(wordList);
 
-            List<String> upList = wordList.subList(0, separator);
+            List<Map.Entry<String, Integer>> upList = wordList.subList(0, separator);
             appendBit(result, upList, true);
-            List<String> downList = wordList.subList(separator, wordList.size());
+            List<Map.Entry<String, Integer>> downList = wordList.subList(separator, wordList.size());
             appendBit(result, downList, false);
         }
     }
+
+    private int getSeparator(List<Map.Entry<String, Integer>> list)
+    {
+        int sum = 0;
+        for (Map.Entry<String, Integer> word : list)
+            sum+=word.getValue();
+
+        int halfSum = 0;
+        int idx = 0;
+        while(halfSum < sum/2)
+        {
+            halfSum += list.get(idx).getValue();
+            idx++;
+        }
+        return idx;
+    }
+
 
     private void saveEncodedFile(URL sourceFilepath) throws IOException, URISyntaxException {
         File file = new File(getFileName(sourceFilepath));
@@ -148,7 +159,7 @@ public class Encode {
                 FileOutputStream(getFileName(sourceFilepath)));
 
         //Construct and write header
-        String metaData = decodedTextRoot + compressedResult.toString().trim().replace(" ", "");
+        String metaData = decodedTextRoot + codeTable.toString().trim().replace(" ", "");
         writer.writeBytes(metaData);
 
         //iterate through whole input and encode it
@@ -170,7 +181,7 @@ public class Encode {
                 remainder += '0';
             }
 
-            Byte b = (byte) Integer.parseInt(remainder, 2); //Byte.parseByte("01111111", 2);
+            byte b = (byte) Integer.parseInt(remainder, 2); //Byte.parseByte("01111111", 2);
             writer.writeByte(b);
         }
         writer.writeByte(lastByteZeroCount);
@@ -181,18 +192,17 @@ public class Encode {
 
     //returns remainder
     private String writeEncodedBytesBuffer(String input, DataOutputStream writer, String remainder) throws IOException {
-        String output = remainder;
+        StringBuilder outputSb = new StringBuilder(remainder);
         // encode the input string
-        boolean isRemainderInputStringLeft = false;
         for (int i = 0; i < input.length(); i += wordLength) {
             if (input.length() < i + wordLength)
             {   //should only happen at last buffer iteration
                 break;
             }
-
             String word = input.substring(i, i + wordLength);
-            output += compressedResult.get(word);
+            outputSb.append(codeTable.get(word));
         }
+        String output = outputSb.toString();
 
         // convert encoded string symbols to bytes and write them to stream
         for (int i = 0; i < output.length(); i += 8) {
@@ -201,7 +211,7 @@ public class Encode {
                 return output.substring(i);
             }
             String bitString = output.substring(i, i + 8);
-            Byte b = (byte) Integer.parseInt(bitString, 2);
+            byte b = (byte) Integer.parseInt(bitString, 2);
             writer.writeByte(b);
         }
         return ""; //no reminder
